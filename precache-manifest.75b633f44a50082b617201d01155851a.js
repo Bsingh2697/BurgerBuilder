49b099b8a362ffa47604a43f49d6d68a946f58f7
Bharat <singh2697.bs@gmail.com>
2019-12-25 02:27:22 +0530

    Updates

---
+++ precache-manifest.75b633f44a50082b617201d01155851a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b97632c396a64618fec683ef235c59",
+    "url": "/Bsingh2697/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "0ac59ad37d01dc4ccc3e",
+    "url": "/Bsingh2697/BurgerBuilder/static/css/main.337dcffb.chunk.css"
+  },
+  {
+    "revision": "f01e456189da5c4b4f71",
+    "url": "/Bsingh2697/BurgerBuilder/static/js/2.9e934bd3.chunk.js"
+  },
+  {
+    "revision": "0ac59ad37d01dc4ccc3e",
+    "url": "/Bsingh2697/BurgerBuilder/static/js/main.13ee585d.chunk.js"
+  },
+  {
+    "revision": "44da57d0acb602d4955e",
+    "url": "/Bsingh2697/BurgerBuilder/static/js/runtime-main.172909f7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Bsingh2697/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
